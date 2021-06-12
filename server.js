72f7eb056f81c1e5c7763aabdb8c08fda4c57283
James McMorran <jtmcmorran@gmail.com>
2021-06-12 10:46:39 -0400

    tip calculator express application created

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/greeting/:name', (req, res) =>{
   res.send(`<h1>greetings ${req.params.name}</h1>`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+  res.send(`<h1>${req.params.total*(req.params.tipPercentage/100)}</h1>`)
+})
 app.listen(PORT, () => {
   console.log("listening on port ", PORT)
 })
